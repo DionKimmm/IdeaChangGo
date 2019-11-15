bf21d6110563cecb0a07eed19ed585b49054162c
dionKim__ <ktqh9535@gmail.com>
2019-11-15 19:05:30 +0900

    tttt

---
+++ ij_temp/src/com/company/Test.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Test extends JFrame {
+    public Test(){
+
+    }
+
+    static int tttt(String a, String b, String c){
+        System.out.println(a + "");
+
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println("siso");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        Test.tttt("a", "b", "c");
+    }
+}
